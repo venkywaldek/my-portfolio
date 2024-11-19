d6a18baf2a4780da353d92fd08e205e7c224d71d
venkywaldek <venkatneoiit@gmail.com>
2024-11-19 20:40:57 +0200

    second commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
+import MagicButton from './ui/MagicButton';
 
 const Hero = () => {
   return (
@@ -28,12 +29,16 @@ const Hero = () => {
             Dynamic Web Magic with Next.js
           </h2>
           <TextGenerateEffect
-            className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words='Transforming Concepts into Seamless Experiences'
+            className=' text-[40px] md:text-5xl lg:text-6xl font-bold'
+            words="Transforming Concepts into Seamless Experiences"
+
           />
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
             Hi, I&apos;m Venkat Kumar, a Next.js Developer based in Finland
           </p>
+          <a href="#about">
+          <MagicButton title = "Show my work" />
+          </a>
         </div>
       </div>
     </div>

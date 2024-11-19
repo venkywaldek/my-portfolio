d13bdc5d95327eb5f7e9c829363ba06fa56a060c
venkywaldek <venkatneoiit@gmail.com>
2024-11-19 20:52:25 +0200

    third commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Hero = () => {
   return (
@@ -12,11 +13,11 @@ const Hero = () => {
           fill='white'
         />
         <Spotlight
-          className='top-10 left-full h-[80vh] w-[50vw]'
+          className="h-[80vh] w-[50vw] top-10 left-full"
           fill='purple'
         />
         <Spotlight
-          className='top-28 left-80 md:-left-32 h-[80vh] w-[50vw]'
+          className=' left-80 top-28 h-[80vh] w-[50vw]'
           fill='blue'
         />
       </div>
@@ -25,19 +26,22 @@ const Hero = () => {
       </div>
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 '>
+          <p className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 '>
             Dynamic Web Magic with Next.js
-          </h2>
+          </p>
           <TextGenerateEffect
-            className=' text-[40px] md:text-5xl lg:text-6xl font-bold'
-            words="Transforming Concepts into Seamless Experiences"
-
+            className=' text-center text-[40px] md:text-5xl lg:text-6xl'
+            words='Transforming Concepts into Seamless Experiences'
           />
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            Hi, I&apos;m Venkat Kumar, a Next.js Developer based in Finland
+            Hi, I&apos;m Venkat Kumar, a Next.js Developer based in Finland.
           </p>
-          <a href="#about">
-          <MagicButton title = "Show my work" />
+          <a href='#about'>
+            <MagicButton
+              title='Show my work'
+              icon={<FaLocationArrow />}
+              position='right'
+            />
           </a>
         </div>
       </div>

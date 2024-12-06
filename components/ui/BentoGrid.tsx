a7c28a8ab16aa655ff4d226e425e267106838829
Venkat <venkat@Venkats-MacBook-Air.local>
2024-12-06 17:02:15 +0200

    bentogrid items debugging

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,14 @@
+'use client';
 import { cn } from '@/utils/cn';
 import { BackgroundGradientAnimation } from './GradientBg';
+import { GlobeDemo } from './GridGlobe';
+import { div, span } from 'framer-motion/client';
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import animationData from '@/data/confetti.json';
+import { IoCopyOutline } from 'react-icons/io5';
+import MagicButton from './MagicButton';
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 
 export const BentoGrid = ({
   className,
@@ -11,7 +20,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        'grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
+        'grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7  gap-4 lg:gap-8 mx-auto ',
         className
       )}
     >
@@ -22,9 +31,9 @@ export const BentoGrid = ({
 
 export const BentoGridItem = ({
   className,
+  id,
   title,
   description,
-  id,
   img,
   imgClassName,
   titleClassName,
@@ -39,10 +48,16 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText('venkatneoiit@gmail.com');
+
+    setCopied(true);
+  };
   return (
     <div
       className={cn(
-        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border-transparent justify-between flex flex-col space-y-4 border ',
+        'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]',
         className
       )}
       style={{
@@ -89,11 +104,63 @@ export const BentoGridItem = ({
           <div className='font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10'>
             {description}
           </div>
-          <div
-            className='font-sans font-bold text-lg  lg:text-3xl max-w-96 z-10'
-          >
+          <div className='font-sans font-bold text-lg  lg:text-3xl max-w-96 z-10'>
             {title}
           </div>
+
+          {id === 2 && <GlobeDemo />}
+
+          {id === 3 && (
+            <div className='flex gap-1 lg:gap-5 w-fit absolut -right-3 lg:-right-2'>
+              <div className='flex flex-col gap-3 lg:gap-8'>
+                {['React.js', 'Next.js', 'TypeScript.js'].map((item) => (
+                  <span
+                    key={item}
+                    className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]'
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]]' />
+              </div>
+
+              <div className='flex flex-col gap-3 lg:gap-8'>
+                <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]]' />
+                {['Angular', 'Azure', 'MongoDB'].map((item) => (
+                  <span
+                    key={item}
+                    className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]'
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className='mt-5 relative '>
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: 'xMidYMid slice',
+                    },
+                  }}
+                />
+              </div>
+              <MagicButton
+                title={copied ? 'Email copied' : 'Copy my email'}
+                icon={IoCopyOutline }
+                position='left'
+                otherClasses='!bg-[#161a31]'
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
